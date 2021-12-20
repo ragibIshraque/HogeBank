6b93d53bed0a26bbb9b7f7f4ed704c87cd5e02e7
Ragib Ishraque <ragibsust@gmail.com>
2021-12-20 16:56:20 +0900

    RegistrationPage POM update

--- src/main/java/org/anymindgroup/ragib/pages/Registration.java
+++ src/main/java/org/anymindgroup/ragib/pages/Registration.java
@@ -1,4 +1,16 @@
 package org.anymindgroup.ragib.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class Registration {
+    WebDriver driver;
+    private By errorMSG = By.xpath("//span");
+
+    public Registration(WebDriver driver) {
+        this.driver = driver;
+    }
+    public String  getErrorMessage(){
+        return driver.findElement(errorMSG).getText();
+    }
 }
