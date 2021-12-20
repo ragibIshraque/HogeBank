69f41032d418e1d2e284e213552c0c8ad208ee91
Ragib Ishraque <ragibsust@gmail.com>
2021-12-20 16:15:57 +0900

    Update Constant.java

--- src/main/java/org/anymindgroup/ragib/util/Constant.java
+++ src/main/java/org/anymindgroup/ragib/util/Constant.java
@@ -5,4 +5,10 @@ public class Constant {
     public static final String LOGIN_URL = "https://anylogi-recruitment.web.app/login";
     public static final String DEPOSIT_URL = "https://anylogi-recruitment.web.app/deposit";
     public static final String WITHDRAQ_URL = "https://anylogi-recruitment.web.app/withdraw";
+    public static final String ERROR_MESSAGE_USER_BLANK = "User name cannot be blank";
+    public static final String ERROR_MESSAGE_USER_WHITESPACE = "User name cannot contain whitespaces";
+    public static final String ERROR_MESSAGE_PASSWORD_LESS = "Password cannot be less than 8 characters";
+    public static final String ERROR_MESSAGE_PASSWORD_MORE = "Password cannot be longer than 32 characters";
+    public static final String ERROR_MESSAGE_PASSWORD_NUMBER = "Password must contain numbers";
+    public static final String ERROR_MESSAGE_PASSWORD_UPPER_CASE = "Password must contain uppercase letters";
 }
